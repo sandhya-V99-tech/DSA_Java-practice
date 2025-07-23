a7481d9695fcf8d063967ea5963805e6a94398fa
Sandhya V <sandhyaddiyaa9999@gmail.com>
2025-07-23 11:08:19 +0530

    Implemented array rotation logic

---
+++ Array/rotateArray.java
@@ -0,0 +1,15 @@
+public class rotateArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+
+        int last = arr[n - 1];
+        for (int i = n - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = last;
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+}
